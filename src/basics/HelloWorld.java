30dbf5bd44a36848e55f18e072ccc483757dfede
DJBritt <dalejbrittain@gmail.com>
2018-01-30 09:50:30 -0700

    Update HelloWorld.java

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main (String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("This is a new line that was just added");
+		System.out.println("Added another line via Git Hub");
 	}
 
 }
